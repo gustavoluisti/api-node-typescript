9afc4a7447ffad6c29f5185b04630146bbda7b6c
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-04-23 19:28:03 -0300

    Models

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,4 @@
-import { uuid } from 'uuidv4';
+
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity('appointments')
@@ -12,11 +12,6 @@ class Appointment {
     @Column('timestamp with time zone')
     date: Date;
 
-    constructor({provider, date}: Omit<Appointment, 'id'>) {
-        this.id = uuid();
-        this.provider = provider;
-        this.date = date;
-    }
 }
 
 export default Appointment;
