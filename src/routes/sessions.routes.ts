8b4d617d8d3b3d33b56f47b8606577d9cd418614
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-04-25 16:10:15 -0300

    Autenticando Usuário

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../service/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const authenticateUser = new AuthenticateUserService();
+
+        const { user } = await authenticateUser.execute({
+            email,
+            password,
+        });
+
+        delete user.password;
+
+        return res.json({ user });
+    } catch (err) {
+        return res.status(400).json({ err: err.message });
+    }
+});
+
+export default sessionsRouter;
