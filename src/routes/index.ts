0e0b13791ee5548347f5332e31ccbc5447ee514a
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-04-25 14:52:19 -0300

    Criptografia

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,10 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
-
-routes.post('/users', (req, res) => {
-    const { name, email } = req.body;
-
-    const user = {
-        name,
-        email
-    }
-    return res.status(200).json(user);
-});
+routes.use('/users', usersRouter);
 
 export default routes;
