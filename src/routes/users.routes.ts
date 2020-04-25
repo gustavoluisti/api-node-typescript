675d50f8d494b97437100ed9b0f38360830eb629
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-04-25 20:10:43 -0300

    Upload de Avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateuserService from '../service/CreateUserService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
     try {
@@ -26,8 +29,14 @@ usersRouter.post('/', async (req, res) => {
     }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (req, res) => {
-    return res.json({ ok: true })
-})
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (req, res) => {
+        console.log(req.file)
+        return res.json({ ok: true });
+    },
+);
 
 export default usersRouter;
